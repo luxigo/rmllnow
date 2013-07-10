93cd7750b01b1285cbb6e0eb4a7167ef69a9f2bb
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2013-07-10 13:45:29 +0200

    rmllnow.sh

--- rmllnow.sh
+++ rmllnow.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-/usr/bin/wget "http://schedule2013.rmll.info/spip.php?page=rmll_progxml2&lang=fr" -O /var/www/rmllnow/prog.xml
 cd /var/www/rmllnow
-node rmllnow.js > index.html
-node rmllnow.js full=1 > full/index.html
+touch prog.0.xml
+/usr/bin/wget "http://schedule2013.rmll.info/spip.php?page=rmll_progxml2&lang=fr" -O prog.xml
+if [ $(diff prog.xml prog.0.xml | wc -l) -gt 0 ] ; then
+  node rmllnow.js > index.html.tmp && mv index.html.tmp index.html
+  node rmllnow.js full=1 > full/index.html.tmp && mv full/index.html.tmp full/index.html
+  mv prog.xml prog.0.xml
+fi
 
