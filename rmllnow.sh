886a0b9efd4e289c50e8f6b7156af6c23aa90c37
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2014-07-08 09:42:48 +0200

    wget --no-check-certificate, dont try to update when wget failed

--- rmllnow.sh
+++ rmllnow.sh
@@ -2,17 +2,19 @@
 URL="https://$(date +%Y).rmll.info/spip.php?page=cfp_progxml&lang="
 cd /var/www/rmllnow
 touch prog.fr.xml
-/usr/bin/wget ${URL}"fr" -O prog.xml
-if [ $(diff prog.xml prog.fr.xml | wc -l) -gt 0 ] ; then
-  node rmllnow.js > index.html.tmp && mv index.html.tmp index.html
-  node rmllnow.js full=1 > full/index.html.tmp && mv full/index.html.tmp full/index.html
-  mv prog.xml prog.0.xml
+if /usr/bin/wget --no-check-certificate ${URL}"fr" -O prog.xml ; then
+  if [ $(diff prog.xml prog.fr.xml | wc -l) -gt 0 ] ; then
+    node rmllnow.js > index.html.tmp && mv index.html.tmp index.html
+    node rmllnow.js full=1 > full/index.html.tmp && mv full/index.html.tmp full/index.html
+    mv prog.xml prog.0.xml
+  fi
 fi
 touch prog.en.xml
-/usr/bin/wget ${URL}"en" -O prog.xml
-if [ $(diff prog.xml prog.en.xml | wc -l) -gt 0 ] ; then
-  node rmllnow.js lang=en > index.en.html.tmp && mv index.en.html.tmp index.en.html
-  node rmllnow.js lang=en\&full=1 > full/index.en.html.tmp && mv full/index.en.html.tmp full/index.en.html
-  mv prog.xml prog.en.xml
+if /usr/bin/wget --no-check-certificate ${URL}"en" -O prog.xml ; then
+  if [ $(diff prog.xml prog.en.xml | wc -l) -gt 0 ] ; then
+    node rmllnow.js lang=en > index.en.html.tmp && mv index.en.html.tmp index.en.html
+    node rmllnow.js lang=en\&full=1 > full/index.en.html.tmp && mv full/index.en.html.tmp full/index.en.html
+    mv prog.xml prog.en.xml
+  fi
 fi
 
